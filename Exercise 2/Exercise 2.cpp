6dab4a0ad7896946799f54b3e10e71c5df235422
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-12 16:00:55 +0800

    memasukan int dan if di dalam while

--- Exercise 2/Exercise 2.cpp
+++ Exercise 2/Exercise 2.cpp
@@ -7,4 +7,26 @@ int high;
 void BinarySearch() 
 {
 	while (low <= high)
+	{
+		int mid = low = (high - low) / 2;
+		if (Faris[97] == mid)
+			return;
+		if (Faris[97] > mid) {
+			low = mid + 1;
+		}
+		if (Faris[97] > mid) {
+			high = mid -1;
+		}
+	}
+	return; -1;
+}
+int main()
+{
+	int Faris[] = {2,3,4,5,5,10,12,20,97};
+	int n = 97;
+	int result = (97, 0, n - 1, 20);
+	if (result == -1);
+}
+
+
 	
